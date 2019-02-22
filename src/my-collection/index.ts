b18e48e08991774b99719d8ac6980db18d345359
sis0k0 <stanimira.vlaeva@gmail.com>
2019-02-23 01:43:07 +0530

    :purple_heart: initial commit

---
+++ src/my-collection/index.ts
@@ -0,0 +1,15 @@
+import { Rule, Tree } from '@angular-devkit/schematics';
+
+export function myCollection(): Rule {
+  return (tree: Tree) => {
+    const buffer = tree.read('./package.json');
+    const content = buffer! .toString();
+    const json = JSON.parse(content);
+
+    json.dependencies["my-new-library"] = "0.15.0";
+
+    tree.overwrite('./package.json', JSON.stringify(json, null, 2));
+    return tree;
+  };
+}
+
